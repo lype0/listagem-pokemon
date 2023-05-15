0dd11880ef221c58d18509d9b50b55a043b09214
lype0 <125227175+lype0@users.noreply.github.com>
2023-05-15 16:10:44 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const body = document.querySelector("body");
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const imagemBotao = document.querySelector(".imagem-botao");
+let modoEscuroAtivo = localStorage.getItem("temaEscuroAtivo");
+
+if (modoEscuroAtivo === "true") {
+  body.classList.add("modo-escuro");
+  imagemBotao.setAttribute("src", "./src/imagens/moon.png");
+}
+
+botaoAlterarTema.addEventListener("click", () => {
+  if (!modoEscuroAtivo) {
+    body.classList.add("modo-escuro");
+    imagemBotao.setAttribute("src", "./src/imagens/moon.png");
+    localStorage.setItem("temaEscuroAtivo", true);
+    modoEscuroAtivo = true;
+  } else {
+    body.classList.remove("modo-escuro");
+    imagemBotao.setAttribute("src", "./src/imagens/sun.png");
+    localStorage.setItem("temaEscuroAtivo", false);
+    modoEscuroAtivo = false;
+  }
+});
+ 
+fetch("https://pokeapi.co/api/v2/").then(response => {
+  return response.json();
+}).then(data => {
+  console.log("data", data);
+});
+
